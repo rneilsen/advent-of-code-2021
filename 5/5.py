4ad5596c7a92f899c7397151f8bd15c0438f6efc
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-12-06 14:22:09 +1100

    Completed day 5

--- 5/5.py
+++ 5/5.py
@@ -16,26 +16,31 @@ for row in rows:
     start, end = row.split(' -> ')
     vents.append(Vent(start.split(',')[::-1], end.split(',')[::-1]))
 
-print(vents)
 
-def find_crossings(vents: list[Vent]):
+def find_crossings(vents: list[Vent], count_diags=False):
     num_rows = max([max(vent.start_x, vent.end_y) for vent in vents]) + 1
     num_cols = max([max(vent.start_y, vent.end_y) for vent in vents]) + 1
     
     crossings = [[0 for _ in range(num_cols)] for _ in range(num_rows)]
+    calc_step = lambda x, y: (0 if x == y else (1 if x > y else -1))
 
     for vent in vents:
-        x_terms = {vent.start_x, vent.end_x}
-        y_terms = {vent.start_y, vent.end_y}
-        if vent.start_x == vent.end_x:
-            i = vent.start_x
-            for j in range(min(y_terms), max(y_terms) + 1):
-                crossings[i][j] += 1
-        elif vent.start_y == vent.end_y:
-            j = vent.start_y
-            for i in range(min(x_terms), max(x_terms) + 1):
-                crossings[i][j] += 1
+        if not count_diags and vent.start_x != vent.end_x and vent.start_y != vent.end_y:
+            continue
+
+        x, y = vent.start_x, vent.start_y
+        x_step = calc_step(vent.end_x, vent.start_x)
+        y_step = calc_step(vent.end_y, vent.start_y)
+        x_range = range(min(vent.start_x, vent.end_x), max(vent.start_x, vent.end_x) + 1)
+        y_range = range(min(vent.start_y, vent.end_y), max(vent.start_y, vent.end_y) + 1)
+
+        while x in x_range and y in y_range:
+            crossings[x][y] += 1
+            x += x_step
+            y += y_step
     
     return sum([len([x for x in row if x > 1]) for row in crossings])
 
+
 print(f'Part 1: {find_crossings(vents)} crossings')
+print(f'Part 2: {find_crossings(vents, count_diags=True)} crossings')
