06d9943136b9734f00c497dd152e0cfd8a651789
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-12-16 14:13:07 +1100

    Complete day 15, part 2

--- 15/15.py
+++ 15/15.py
@@ -1,56 +1,72 @@
 import time
+from heapq import heapify, heappop, heappush
 
 start_time = time.time()
+
+TILES = 5
+REMOVED = -1
 
 with open('input') as f:
     map = [[int(ch) for ch in list(row.strip())] for row in f.readlines()]
 
-TILES = 5
-
-def find_best_path(map):
+def find_lowest_risk_path(map):
     height = len(map)
     width = len(map[0])
     target = (height - 1, width - 1)
 
-    unvisited = {(i, j) for i in range(height) for j in range(width)}
-    visited = set()
-    best_distance = {node: float('inf') for node in unvisited}
-    best_distance[(0,0)] = 0
+    frontier = [(map[0][1], 0, 1), (map[1][0], 1, 0)]
+    heapify(frontier)
 
-
-    def get_neighbours(i, j):
+    def get_neighbours(x, y):
+        """Returns a set of the valid coordinate pairs of all adjacent cells
+        to the given coordinates"""
         neighbours = set()
-        if i > 0:
-            neighbours.add((i-1, j))
-        if i < height - 1:
-            neighbours.add((i+1, j))
-        if j > 0:
-            neighbours.add((i, j-1))
-        if j < width - 1:
-            neighbours.add((i, j+1))
+        if x > 0:
+            neighbours.add((x-1, y))
+        if x < height - 1:
+            neighbours.add((x+1, y))
+        if y > 0:
+            neighbours.add((x, y-1))
+        if y < width - 1:
+            neighbours.add((x, y+1))
         return neighbours
 
 
-    def get_min_distance(node_set):
-        min_distance = float('inf')
-        for node in node_set:
-            if best_distance[node] < min_distance:
-                min_distance = best_distance[node]
-                best_node = node
-        return best_node
+    def find_in_frontier(x, y):
+        """Finds a tuple of the index and current priority of the given 
+        coordinate pair in the frontier, or None if the given 
+        coordinate pair is not in the frontier"""
+        for i in range(len(frontier)):
+            fr_p, fr_x, fr_y = frontier[i]
+            if fr_x == x and fr_y == y:
+                return (i, fr_p)
+        return None
+            
+   
+    visited = set()
 
+    while target not in visited:
+        while True:
+            cur_risk, x, y = heappop(frontier)
+            if x != REMOVED:
+                break
+        visited.add((x, y))
+        if (x, y) == target:
+            return cur_risk
 
-    current_node = (0,0)
-    while best_distance[target] == float('inf'):
-        for i, j in get_neighbours(*current_node) - visited:
-            best_distance[(i, j)] = min(best_distance[(i, j)], best_distance[current_node] + map[i][j])
-        visited.add(current_node)
-        unvisited.remove(current_node)
-        current_node = get_min_distance(unvisited)
+        neighbours = get_neighbours(x, y) - visited
+        for n_x, n_y in neighbours:
+            if (fif := find_in_frontier(n_x, n_y)) is not None:
+                n_i, n_p = fif
+                if cur_risk + map[n_x][n_y] < n_p:
+                    frontier[n_i] = (n_p, REMOVED, REMOVED)
+                    heappush(frontier, (cur_risk + map[n_x][n_y], n_x, n_y))
+            else:
+                heappush(frontier, (cur_risk + map[n_x][n_y], n_x, n_y))
 
-    return best_distance[target]
-
-print('Part 1:', find_best_path(map), f'in {time.time() - start_time:0.2}s')
+print('Part 1:', find_lowest_risk_path(map), 
+        f'({time.time() - start_time:0.2f} s)')
+start_time = time.time()
 
 def inc(row):
     return [(val + 1 if val < 9 else 1) for val in row]
@@ -65,4 +81,5 @@ for _ in range(TILES - 1):
     new_rows = [inc(row) for row in new_rows]
     map.extend(new_rows)
 
-# print('Part 2:', find_best_path(map))
+print('Part 2:', find_lowest_risk_path(map), 
+        f'({time.time() - start_time:0.2f} s)')
