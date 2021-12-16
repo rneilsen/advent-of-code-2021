564b39f7b85b74693f87d20219df58feb970b511
Richard Neilsen <RNeilsen@users.noreply.github.com>
2021-12-16 13:30:25 +1100

    Complete day 14, part 2

--- 14/14.py
+++ 14/14.py
@@ -1,27 +1,41 @@
 from collections import Counter
 
-NUM_STEPS = 10
-PRINT_STEPS = [0, 1, 2, 3, 4]
+PRINT_STEPS = (10, 40)
 
 with open('input') as f:
     polymer = f.readline().strip()
     f.readline()
     rows = [row.strip().split(' -> ') for row in f.readlines()]
 
-rules = {pair: addition for pair, addition in rows}
+# 'CH -> B' translates to rules dict as {'CH': ('CB', 'BH')}
+rules = {pair: (pair[0] + addition, addition + pair[1]) for pair, addition in rows}
+
+pair_counts = Counter([polymer[i:i+2] for i in range(len(polymer) - 1)])
 
 n = 0
-while n < NUM_STEPS:
-    if n in PRINT_STEPS:
-        print(f'After step {n}: {polymer}')
+while n < max(PRINT_STEPS):
     n += 1
 
-    new_polymer = ''
-    for i in range(len(polymer) - 1):
-        new_polymer += polymer[i] + rules[polymer[i] + polymer[i+1]]
-    new_polymer += polymer[-1]
-    
-    polymer = new_polymer
+    new_pair_counts = Counter()
+    for pair, count in pair_counts.items():
+        for result in rules[pair]:
+            new_pair_counts[result] += count
 
-counts = Counter(polymer).most_common()
-print('Part 1:', counts[0][1] - counts[-1][1])
+    pair_counts = new_pair_counts
+
+    if n in PRINT_STEPS:
+        letter_counts = Counter()
+        for pair, count in pair_counts.items():
+            for letter in pair:
+                letter_counts[letter] += count
+
+        # all letters are doublecounted since they're counted in every pair
+        for letter in letter_counts:
+            letter_counts[letter] //= 2
+        
+        # adjust first and last letters since they're only counted in one pair
+        letter_counts[polymer[0]] += 1
+        letter_counts[polymer[-1]] += 1
+
+        common = letter_counts.most_common()
+        print(f'After {n} steps:', common[0][1] - common[-1][1])
